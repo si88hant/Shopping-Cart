1f84bb9dd482f15e9cb890724cf5f0b700037ed5
Siddhant Bhalla <83804768+si88hant@users.noreply.github.com>
2021-05-25 11:04:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import CartItem from "./CartItem";
+
+export default function App() {
+  return (
+    <div className="App">
+      <CartItem />
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import "/public/index.css";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  rootElement
+);
