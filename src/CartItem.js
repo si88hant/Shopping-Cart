92a256919677e5c822f3bb00c7db235fcdd93a48
Siddhant Bhalla <83804768+si88hant@users.noreply.github.com>
2021-05-26 11:29:31 +0530

    modify state

--- src/CartItem.js
+++ src/CartItem.js
@@ -13,7 +13,19 @@ class CartItem extends React.Component {
   }
 
   increaseQuantity = () => {
-    console.log("this", this.state);
+    // console.log("this", this.state);
+
+    // //setState form 1
+    // this.setState({
+    //   qty: this.state.qty + 1
+    // });
+
+    //setState form 2  - if prevState required, use this
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty + 1
+      }
+    });
   };
   render() {
     const { price, title, qty } = this.state; //object destructuring
